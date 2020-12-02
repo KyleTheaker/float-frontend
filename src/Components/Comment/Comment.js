fb3ab385c02cdd42984d41565c25b800333d4e84
jasperwreed <jmixon.reed@gmail.com>
2020-12-02 10:32:00 -0800

    modal created for posts

---
+++ src/Components/Comment/Comment.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Comment extends Component {
+    render() {
+        console.log(this.props)
+        return (
+            <div>
+                <p>{this.props.comment.text}</p>
+            </div>
+        )
+    }
+}
+
+export default Comment
--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { createLike } from '../../Actions/likeActions'
 import { Link } from 'react-router-dom'
+import { Modal } from 'react-bootstrap'
+import Comment from '../Comment/Comment'
 
 class Post extends Component {
 
     state = {
         post_id: '',
         user_id: '',
-        likes: this.props.post.likes.length
+        likes: this.props.post.likes.length,
+        show: false
+    }
+
+    renderComments = () => {
+        if (this.props.post.comments.length !== 0) {
+            return this.props.post.comments.map( comment => <Comment comment={comment} />)
+        }
     }
 
     handleLikes = (post_id) => {
@@ -27,19 +36,46 @@ class Post extends Component {
         })
     }
 
+    handleClose = () => {
+        this.setState({
+            ...this.state,
+            show: false
+        })
+    }
+
+    handleShow = () => {
+        this.setState({
+            ...this.state,
+            show: true
+        })
+    }
+
     render() {
+        console.log(this.props.post)
         return (
-            <div className='card shadow p-3 mb-5 bg-transparent rounded'>
-                <img src={this.props.post.image} className='card-img rounded' alt=''/>
-                <div className='card-body'>
-                    <p>{this.props.post.text}</p>
-                    <p>{this.state.likes} <span onClick={() => this.handleLikes(this.props.post_id)}>♡</span></p>
-                    <footer className='blockquote-footer'>
-                        <cite>
-                            <Link key={this.props.post.user.id + 'u'} to={`/users/${this.props.post.user.id}`}>{this.props.post.user.username}</Link>
-                        </cite>
-                    </footer>
+            <div>
+                <div className='card shadow p-3 mb-5 bg-transparent rounded' onClick={() => this.handleShow()}>
+                    <img src={this.props.post.image} className='card-img rounded' alt=''/>
+                    <div className='card-body'>
+                        <p>{this.props.post.text}</p>
+                        <p>{this.state.likes} <span onClick={() => this.handleLikes(this.props.post_id)}>♡</span></p>
+                        <footer className='blockquote-footer'>
+                            <cite>
+                                <Link key={this.props.post.user.id + 'u'} to={`/users/${this.props.post.user.id}`}>{this.props.post.user.username}</Link>
+                            </cite>
+                        </footer>
+                    </div>
                 </div>
+
+                <Modal show={this.state.show} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Header</Modal.Title>
+                    </Modal.Header>
+                    <img src={this.props.post.image} className='card-img rounded' alt=''/>
+                    <Modal.Body>{this.props.post.text}</Modal.Body>
+                    <h5>Comments</h5>
+                    <Modal.Footer>{this.renderComments()}</Modal.Footer>
+                </Modal>
             </div>
         )
     }
